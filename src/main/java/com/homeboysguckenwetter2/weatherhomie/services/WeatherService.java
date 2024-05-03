d2b77498389d6ca118bf04a7e8938448e4420734
iMoe400 <imoe400@gmail.com>
2024-05-03 21:14:54 +0200

    Push: Deleted useless Record "TimeAndTemp" hence HourlyData already retrieves two lists.

--- src/main/java/com/homeboysguckenwetter2/weatherhomie/services/WeatherService.java
+++ src/main/java/com/homeboysguckenwetter2/weatherhomie/services/WeatherService.java
@@ -4,11 +4,6 @@ package com.homeboysguckenwetter2.weatherhomie.services;
 import com.homeboysguckenwetter2.weatherhomie.controller.WeatherController;
 import com.homeboysguckenwetter2.weatherhomie.weatherModel.TempList;
 import com.homeboysguckenwetter2.weatherhomie.weatherModel.TimeList;
-import com.homeboysguckenwetter2.weatherhomie.weatherModel.TimeAndTemp;
-
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class WeatherService {
     WeatherController weatherController = new WeatherController();
@@ -16,12 +11,6 @@ public class WeatherService {
     TempList tempList = new TempList(weatherController.getForecastData().hourly().temperature_2m());
 
     public WeatherService() {
-    }
-
-    public TimeAndTemp getTimeAndTemp() {
-        Map<TimeList, TempList> timeTempMap = new HashMap<>();
-        timeTempMap.put(timeList, tempList);
-        return new TimeAndTemp(timeTempMap);
     }
 
     public void printTimeAndTemp() {
--- src/main/java/com/homeboysguckenwetter2/weatherhomie/weatherModel/TimeAndTemp.java
+++
@@ -1,6 +0,0 @@
-package com.homeboysguckenwetter2.weatherhomie.weatherModel;
-
-import java.util.Map;
-
-public record TimeAndTemp(Map<TimeList, TempList> timeTempMap) {
-}
